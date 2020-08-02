282418915ae64f8d2e436ae9fae5730b1eb07df8
LYU <xiranlyu@foxmail.com>
2020-08-03 00:18:29 +0800

    1.2 test written and implementation passed

--- TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,19 @@
 package com.twu.biblioteca;
 
+
+import java.util.ArrayList;
+
 public class BibliotecaApp {
     public String welcome() {
         return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    }
+
+    public String showListOfBooks(ArrayList<String> listOfBooks){
+        StringBuilder result = new StringBuilder();
+        for (String book: listOfBooks) {
+            result.append(book).append("\n");
+        }
+        return result.toString();
     }
 
     public static void main(String[] args) {
--- TWU_Biblioteca-master/test/com/twu/biblioteca/ExampleTest.java
+++ TWU_Biblioteca-master/test/com/twu/biblioteca/ExampleTest.java
@@ -2,14 +2,31 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
     @Test
-    public void ShowWelcomeMessage() {
+    public void viewWelcomeMessage() {
         String expectedMsg = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
         BibliotecaApp newCustomer = new BibliotecaApp();
         assertEquals(expectedMsg, newCustomer.welcome());
+    }
+
+    @Test
+    public void viewListOfBooks() {
+        ArrayList<String> listOfBooks = new ArrayList<String>();
+        listOfBooks.add("Head First Java");
+        listOfBooks.add("Effective Java");
+        listOfBooks.add("Core Java");
+        StringBuilder result = new StringBuilder();
+        for (String book: listOfBooks) {
+            result.append(book).append("\n");
+        }
+        BibliotecaApp newCustomer = new BibliotecaApp();
+        assertEquals(result.toString(), newCustomer.showListOfBooks(listOfBooks));
     }
 }
