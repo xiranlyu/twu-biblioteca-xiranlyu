4cbf1faa60408cb0c4129e24f7f7abc0cba59103
LYU <xiranlyu@foxmail.com>
2020-08-02 23:23:06 +0800

    1.1 test written and implementation passed

--- TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,12 @@
 package com.twu.biblioteca;
 
 public class BibliotecaApp {
+    public String welcome() {
+        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp newCustomer = new BibliotecaApp();
+        newCustomer.welcome();
     }
 }
--- TWU_Biblioteca-master/test/com/twu/biblioteca/ExampleTest.java
+++ TWU_Biblioteca-master/test/com/twu/biblioteca/ExampleTest.java
@@ -7,7 +7,9 @@ import static org.junit.Assert.assertEquals;
 public class ExampleTest {
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void ShowWelcomeMessage() {
+        String expectedMsg = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        BibliotecaApp newCustomer = new BibliotecaApp();
+        assertEquals(expectedMsg, newCustomer.welcome());
     }
 }
